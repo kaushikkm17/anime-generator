fc27342da75de942ab8b8d46ebbaaea76b99e241
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2022-12-06 15:18:50 -0500

    retrieve data and output to dom

---
+++ README.md
@@ -0,0 +1,13 @@
+# Anime Generator
+## Generates a random anime or character 
+### Technologies used
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- Kitsu API https://kitsu.docs.apiary.io/
+
+
+ Screenshot(s): Images of your actual app.
+ Getting Started: In this section include the link to your deployed app and any instructions you deem important.
+ Future Enhancements: Features you plan to implement later.
--- app.js
+++ app.js
@@ -1,2 +1,42 @@
 // wrap in an IIFE
 
+//***DOM Elements***//
+const $nav = $('#navbar')
+const $showBtn = $('#show-btn')
+const $charBtn = $('#char-btn')
+const $mainHeader = $('#main-header')
+const $form = $('form')
+const $retrievedImg = $('#retrieved-img')
+const $retrievedData = $('#retrieved-data')
+
+//***Functions***//
+
+
+//***Event Listeners***//
+
+//when button is clicked, get data and put in dom
+$('#search-form').on('submit', (e) => {
+    e.preventDefault()
+    const $input = $('#search-form input').val()
+    const fixedInput = $input.split(' ').join('%20')
+    $.ajax({
+        url: `https://kitsu.io/api/edge/anime?filter[text]=${fixedInput}`
+    })
+    .then(
+        data => {
+            console.log(data.data[0].attributes.coverImage.original)
+            $retrievedImg.html(
+                `<img src=
+                ${data.data[0].attributes.coverImage.original}>`)
+        },
+        error => console.log(error)
+    )
+    
+    
+})
+
+
+
+//**planning**//
+//get data to output when someone clicks the submit button based on input
+
